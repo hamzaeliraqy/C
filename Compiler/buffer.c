12d5afa48d32542c60f11f40cf2932fb8c7db91a
hamzaeliraqy <114699172+hamzaeliraqy@users.noreply.github.com>
2022-11-01 16:40:25 -0400

    Update buffer.c

--- Compiler/buffer.c
+++ Compiler/buffer.c
@@ -1,14 +1,9 @@
-/*************************************************************
-* COMPILERS COURSE - Algonquin College
-* Code version: Fall, 2020
-* This Code is EXCLUSIVE for professors (must not be shared)
-* Author: Svillen Ranev - Paulo Sousa - Abdulah.
-*************************************************************
+/**************************************************************
 * File name: buffer.c
 * Compiler: MS Visual Studio 2019
 * Author: Hamza Eliraqy
 * ID : 040976448
-* Course: CST 8152 – Compilers, Lab Section: 011
+* Course: CST 8152 â€“ Compilers, Lab Section: 011
 * Assignment: A1.
 * Date: Feb 20 2021
 * Professor: Paulo Sousa / Abdulah
@@ -35,50 +30,7 @@ bSetMarkOffset, bFinish, bDisplay, bLoad, bIsEmpty, bGetCh, bRewind, bRetract, b
 **************************************************************/
 bPointer bCreate(short size, char increment, char mode) {
 
-	/*bPointer b = NULL;
-	char bMode = (-2);
-
-	if (size <  0 || size > MAX_SIZE) {
-		return NULL;
-	}
-
-	if (size == 0) {
-		size = DEFAULT_SIZE;
-		increment = DEFAULT_INCREMENT;
-	}
-
-	switch (mode) {
-
-	case 'f':
-		bMode = FIXMODE;
-		increment = 0;
-		break;
-	case 'a':
-		bMode = ADDMODE;
-		increment = 255;
-		break;
-	case 'm':
-		bMode = MULMODE;
-		increment = MAXINCREMENT;
-		break;
-	default:
-		return NULL;
-	}
-
-	b = (bPointer)calloc(1, sizeof(bStructure));
-	if (!b) {
-		return NULL;
-	}
-	b->content = (char*)malloc(size);
-
-	if (!b->content) {
-		free(b);
-		return NULL;
-	}
-	b->flags = DEFAULT_FLAGS;
-	b->mode = bMode;
-	b->increment = increment;
-	b->size = size;*/
+	
 
 	bPointer b;
 	char bMode;
@@ -175,58 +127,7 @@ bPointer bAddCh(bPointer const pBuffer, char ch) {
 		pBuffer->size = newSize;
 	}
 	pBuffer->content[pBuffer->addCOffset++] = ch;
-	/*
-	short tempSize = 0;
-	short tempSpace = 0;
-	short tempInc = 0;
-	char* tempContent = NULL;
-	if (!pBuffer) {
-		return NULL;
-	}
-	pBuffer->flags = pBuffer->flags & RESET_R_FLAG;
-	if (bIsFull(pBuffer)) {
-		if (pBuffer->mode == FIXMODE) {
-			return NULL;
-
-		}
-		else if (pBuffer->mode == ADDMODE) {
-			tempSize =  pBuffer->size +(short) bGetIncrement(pBuffer);
-			tempContent = (char*)realloc(pBuffer->content, tempSize);
-			if (tempSize < 0) {
-				return NULL;
-
-			}
-			if (tempSize == SHRT_MAX) {
-				tempSize = (SHRT_MAX -1);
-			}
-
-		}
-		else if (pBuffer->mode == MULMODE) {
-			tempSpace = MAX_SIZE - pBuffer->size;
-			tempInc = ( tempSpace * pBuffer->increment / 100);
-			tempSize = pBuffer->size + tempInc;
-			if (tempSize < 0 || tempSize > MAX_SIZE) {
-				return NULL;
-			}
-		}
-
-		tempContent = (char*)realloc(pBuffer->content, tempSize);
-		if (!tempContent) {
-			return NULL;
-		}
-		if (tempContent != pBuffer->content) {
-			pBuffer->flags = pBuffer->flags | SET_R_FLAG;
-		}
-		pBuffer->content = tempContent;
-
-	}
-
-	if ((pBuffer->size = pBuffer->addCOffset*sizeof(char)) != 0) {
-
-		pBuffer->size = pBuffer->addCOffset * sizeof(char);
-	}
-
-	pBuffer->content[pBuffer->addCOffset++] = ch;*/
+	
 	return pBuffer;
 }
 
@@ -242,14 +143,6 @@ bPointer bAddCh(bPointer const pBuffer, char ch) {
 **************************************************************/
 int bClean(bPointer const pBuffer) {
 
-	/*if (!pBuffer) {
-		return RT_FAIL_1;
-	}
-
-	pBuffer->flags = pBuffer->flags | DEFAULT_FLAGS;
-	pBuffer->getCOffset = 0;
-	pBuffer->addCOffset = 0;
-	pBuffer->markOffset = 0;*/
 	if (!pBuffer)
 		return RT_FAIL_1;
 	pBuffer->addCOffset = pBuffer->markOffset = pBuffer->getCOffset = 0;
@@ -271,11 +164,6 @@ int bClean(bPointer const pBuffer) {
 **************************************************************/
 int bFree(bPointer const pBuffer) {
 
-	/*if (pBuffer->content != NULL) {
-		free(pBuffer->content);
-		pBuffer->content = NULL;
-	}
-	free(pBuffer);*/
 	if (pBuffer) {
 		free(pBuffer->content);
 		free(pBuffer);
@@ -296,12 +184,6 @@ int bFree(bPointer const pBuffer) {
 **************************************************************/
 int bIsFull(bPointer const pBuffer) {
 
-	/*if (!pBuffer) {
-		return RT_FAIL_1;
-	}
-	if (((short)pBuffer->content * (short)sizeof(char)) == (short) pBuffer->size) {
-		return 1;
-	}*/
 	if (!pBuffer)
 		return RT_FAIL_1;
 	return (pBuffer->size == pBuffer->addCOffset * (int)sizeof(char));
@@ -398,11 +280,6 @@ short bGetMarkOffset(bPointer const pBuffer) {
 short bSetMarkOffset(bPointer const pBuffer, short mark) {
 
 
-	/*if (!pBuffer) {
-		return RT_FAIL_1;
-	}
-
-	return pBuffer->markOffset;*/
 	if (!pBuffer || mark<0 || mark > pBuffer->addCOffset)
 		return RT_FAIL_1;
 	pBuffer->markOffset = mark;
@@ -421,27 +298,6 @@ short bSetMarkOffset(bPointer const pBuffer, short mark) {
 **************************************************************/
 bPointer bFinish(bPointer const pBuffer, char ch) {
 
-	/*short tempSize = 0;
-	char* tempContent = NULL;
-	if (!pBuffer) {
-		return NULL;
-	}
-	pBuffer->flags = pBuffer->flags & RESET_R_FLAG;
-
-	tempSize = pBuffer->addCOffset + 1;
-
-		tempContent = (char*)realloc(pBuffer->content, tempSize);
-		if (!tempContent) {
-			return NULL;
-		}
-		if (tempContent != pBuffer->content) {
-			pBuffer->flags = pBuffer->flags | SET_R_FLAG;
-		}
-		pBuffer->content = tempContent;
-
-
-
-	pBuffer->content[pBuffer->addCOffset++] = ch;*/
 	char* tempbuf;
 	short size;
 	if (!pBuffer || pBuffer->addCOffset * sizeof(char) == SHRT_MAX)
@@ -476,23 +332,6 @@ bPointer bFinish(bPointer const pBuffer, char ch) {
 **************************************************************/
 int bDisplay(bPointer const pBuffer, char nl) {
 
-	/*char ch;
-	int numOfChars = 0;
-
-	if (!pBuffer || !pBuffer->content) {
-		return RT_FAIL_1;
-	}
-	while ((pBuffer->flags & CHECK_EOB)) {
-		ch = bGetCh(pBuffer);
-
-		if (nl != 0) {
-			numOfChars++;
-			printf("%c", ch);
-		}
-	}
-
-
-	return numOfChars;*/
 	int cont = 0;
 	char c;
 	if (!pBuffer || !(pBuffer->content))
@@ -521,28 +360,6 @@ int bDisplay(bPointer const pBuffer, char nl) {
 **************************************************************/
 int bLoad(bPointer const pBuffer, FILE* const fi) {
 
-
-	/*char c;
-	int num;
-	num = 0;
-
-	if (pBuffer == NULL || fi == NULL) {
-		return RT_FAIL_1;
-	}
-
-	while (1) {
-		c = (char)fgetc(fi);
-
-		if (feof(fi)) {
-			break;
-		}
-		if (bAddCh(pBuffer, c) == NULL) {
-			ungetc(c, fi);
-			return LOAD_FAIL;
-		}
-		num++;
-	}
-	return num;*/
 	int size = 0;
 	char c;
 	if (!fi || !pBuffer)
@@ -573,13 +390,6 @@ int bLoad(bPointer const pBuffer, FILE* const fi) {
 **************************************************************/
 int bIsEmpty(bPointer const pBuffer) {
 
-	/*if (!pBuffer) {
-		return RT_FAIL_1;
-	}
-	if (pBuffer->addCOffset == 0) {
-		return 1;
-	}
-	return 0;*/
 	if (!pBuffer)
 		return RT_FAIL_1;
 	return (pBuffer->addCOffset == 0);
@@ -598,18 +408,6 @@ int bIsEmpty(bPointer const pBuffer) {
 **************************************************************/
 char bGetCh(bPointer const pBuffer) {
 
-	/*if (!pBuffer) {
-		return RT_FAIL_1;
-	}
-
-	if (pBuffer->getCOffset == pBuffer->addCOffset) {
-		pBuffer->flags |= SET_EOB;
-		return 0;
-	}
-	else {
-		pBuffer->flags &= RESET_EOB;
-	}
-	return pBuffer->content[pBuffer->getCOffset++];*/
 	if (!pBuffer)
 		return RT_FAIL_1;
 	if (pBuffer->getCOffset == pBuffer->addCOffset) {
@@ -731,13 +529,7 @@ size_t bGetIncrement(bPointer const pBuffer) {
 **************************************************************/
 char* bGetContent(bPointer const pBuffer, short chPosition) {
 
-	/*if (!pBuffer) {
-		return NULL;
-	}
-	char* pos;
-	pos = pBuffer->content[chPosition] + pBuffer->markOffset;
 
-	return pos;*/
 	if (!pBuffer || chPosition < 0 || chPosition > pBuffer->addCOffset)
 		return NULL;
 	return pBuffer->content + chPosition;
