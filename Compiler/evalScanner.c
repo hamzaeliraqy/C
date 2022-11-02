856691046a443fe2b7f2f38bd547282718fc87e9
hamzaeliraqy <114699172+hamzaeliraqy@users.noreply.github.com>
2022-11-02 14:06:13 -0400

    Update evalScanner.c

--- Compiler/evalScanner.c
+++ Compiler/evalScanner.c
@@ -1,5 +1,5 @@
 /**************************************************************
-* File name: buffer.c
+* File name: evalScanner.c
 * Compiler: MS Visual Studio 2019
 * Author: Hamza Eliraqy
 *************************************************************/
