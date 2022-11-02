f8c58e92302d5d748156d8bdddb638391c62db15
hamzaeliraqy <114699172+hamzaeliraqy@users.noreply.github.com>
2022-11-02 13:57:17 -0400

    Update buffer.h

--- Compiler/buffer.h
+++ Compiler/buffer.h
@@ -2,21 +2,14 @@
 * File name: buffer.c
 * Compiler: MS Visual Studio 2019
 * Author: Hamza Eliraqy
-* ID : 040976448
-* Course: CST 8152 – Compilers, Lab Section: 011
-* Assignment: A1.
-* Date: Feb 20 2021
-* Professor: Paulo Sousa / Abdulah
 */
 
-// TODO101: Adjust file header
+
 
 #ifndef BUFFER_H_
 #define BUFFER_H_
 
-/*#pragma warning(1:4001) *//*to enforce C89 type comments  - to make //comments an warning */
 
-/*#pragma warning(error:4001)*//* to enforce C89 comments - to make // comments an error */
 
 /* standard header files */
 #include <stdio.h>  /* standard input/output */
@@ -24,34 +17,34 @@
 #include <limits.h> /* implementation-defined data type ranges and limits */
 
 /* constant definitions */
-#define RT_FAIL_1 (-1)			// TODO102
-#define RT_FAIL_2 (-2)			// TODO103
-#define LOAD_FAIL (-2)			// TODO104
+#define RT_FAIL_1 (-1)			
+#define RT_FAIL_2 (-2)			
+#define LOAD_FAIL (-2)			
 
-#define DEFAULT_SIZE 200        // TODO105
-#define DEFAULT_INCREMENT 15   // TODO106
+#define DEFAULT_SIZE 200       
+#define DEFAULT_INCREMENT 15   
 
-/* You should add your own constant definitions here */
+
 #define MAX_SIZE (SHRT_MAX-1)   /* maximum capacity*/ 
 
 /* Buffer Modes */
-#define FIXMODE 0         // TODO107
-#define ADDMODE 1         // TODO108
-#define MULMODE (-1)         // TODO109
+#define FIXMODE 0        
+#define ADDMODE 1        
+#define MULMODE (-1)         
 
-/* Add your bit-masks constant definitions here */
+/* bit-masks constant  */
 #define DEFAULT_FLAGS 0x3FFF 	// 0011.1111 1111.1111
-#define SET_EOB 0x8000          // TODO110  1000.0000.0000.0000
-#define RESET_EOB 0x7FFF        // TODO111  0111.1111.1111.1111
-#define CHECK_EOB 0x8000        // TODO112  1000.0000.0000.0000 // 1100.1111.1111.1111
-#define SET_R_FLAG 0x4000       // TODO113	0100.0000.0000.0000
-#define RESET_R_FLAG 0xBFFF     // TODO114	1011.1111.1111.1111
-#define CHECK_R_FLAG 0x4000     // TODO115	0100.0000.0000.0000
+#define SET_EOB 0x8000          // 1000.0000.0000.0000
+#define RESET_EOB 0x7FFF        // 0111.1111.1111.1111
+#define CHECK_EOB 0x8000        // 1000.0000.0000.0000 // 1100.1111.1111.1111
+#define SET_R_FLAG 0x4000       // 0100.0000.0000.0000
+#define RESET_R_FLAG 0xBFFF     // 1011.1111.1111.1111
+#define CHECK_R_FLAG 0x4000     // 0100.0000.0000.0000
 
 /* Constants used in buffer */
-#define MAXINCREMENT 100 	// TODO116
-#define MAX_VALUE (SHRT_MAX-1) 		// TODO117
-#define RT_INC_FAIL 0x100		// TODO118
+#define MAXINCREMENT 100 	
+#define MAX_VALUE (SHRT_MAX-1) 		
+#define RT_INC_FAIL 0x100		
 
 /* user data type declarations */
 typedef struct Buffer {
@@ -66,7 +59,6 @@ typedef struct Buffer {
 } bStructure, * bPointer;
 
 /* Function declarations */
-// TODO119
 bPointer bCreate(short size, char increment, char mode);
 bPointer bAddCh(bPointer const pBuffer, char ch);
 int bClean(bPointer const pBuffer);
@@ -90,7 +82,6 @@ size_t bGetIncrement(bPointer const pBuffer);
 char* bGetContent(bPointer const pBuffer, short chPosition);
 short bufferAddCPosition(bStructure* const pBuffer);
 unsigned short bGetFlags(bPointer const pBuffer);
-//New definitions
 int bLoad(bPointer const, FILE* const);
 bPointer bRetract(bPointer const);
 short bSetMarkOffset(bPointer const, short);
